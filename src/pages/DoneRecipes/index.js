230d43697d9ef16abe7e72bc76b2126866371d2c
joaovitordantas <joaovitordantasss@hotmail.com>
2022-01-31 17:14:23 -0300

    Add buttons on the done Recipes screen

--- src/pages/DoneRecipes/index.js
+++ src/pages/DoneRecipes/index.js
@@ -3,9 +3,30 @@ import Header from '../../components/Header';
 
 export default function DoneRecipes() {
   return (
-    <div className="done-recipes app-recipes">
+    <div className="done-recipes">
       <Header showSearchButton={ false } />
       <h1 data-testid="page-title">Done Recipes</h1>
+      <div className="main-categories">
+        <input
+          type="button"
+          value="All"
+          data-testid="filter-by-all-btn"
+          className="category"
+        />
+        <input
+          type="button"
+          value="Food"
+          data-testid="filter-by-food-btn"
+          className="category"
+        />
+
+        <input
+          type="button"
+          value="Drink"
+          data-testid="filter-by-drink-btn"
+          className="category"
+        />
+      </div>
     </div>
   );
 }
