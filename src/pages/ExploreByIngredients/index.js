27c960f3d1cae4eb852a048e1446a61fe539b9da
lucca-falbo <luccafalbo@gmail.com>
2022-01-31 14:59:42 -0300

    tela explorar ingredientes

--- src/pages/ExploreByIngredients/index.js
+++ src/pages/ExploreByIngredients/index.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Cards from '../../components/Cards';
+import globalFetch from '../../services/globalFetch';
 
 export default function ExploreByIngredients() {
+  const { push } = useHistory();
+  const { pathname } = useLocation();
+  const [ingredients, setIngredients] = useState([]);
+  const location = pathname.includes('foods') ? 'foods' : 'drinks';
+  const key = location === 'foods' ? 'meals' : 'drinks';
+  const mealsURL = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+  const drinksURL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+
+  useEffect(() => {
+    const URL = location === 'foods' ? mealsURL : drinksURL;
+    const MAX_LENGTH = 12;
+
+    globalFetch(URL)
+      .then((response) => {
+        const ingredientList = response[key];
+        setIngredients(ingredientList.slice(0, MAX_LENGTH));
+      });
+  }, [location, key]);
+
+  function createCards(array) {
+    return array.map(({ idIngredient: id, strIngredient: name }, index) => (
+      <Cards
+        img=""
+        name={ name }
+        key={ id }
+        index={ index }
+        onClick={ () => push('/') }
+      />
+    ));
+  }
+
   return (
-    <div className="explore-drinks-by-ingredients app-recipes">
+    <div className="explore-by-ingredients app-recipes">
       <Header title="Explore Ingredients" showSearchButton={ false } />
+      {createCards(ingredients)}
       <Footer />
     </div>
   );
