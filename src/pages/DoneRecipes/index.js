ce9dfa33c2e66178ce96e44eec5b21f2c3275d61
joaovitordantas <joaovitordantasss@hotmail.com>
2022-02-01 17:52:25 -0300

    DoneRecipes localStorage

--- src/pages/DoneRecipes/index.js
+++ src/pages/DoneRecipes/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import Header from '../../components/Header';
 
 export default function DoneRecipes() {
+  let doneRecipesData = [{
+    id: 'id-da-receita',
+    type: 'comida-ou-bebida',
+    nationality: 'nacionalidade-da-receita-ou-texto-vazio',
+    category: 'categoria-da-receita-ou-texto-vazio',
+    alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
+    name: 'nome-da-receita',
+    image: 'imagem-da-receita',
+    doneDate: 'quando-a-receita-foi-concluida',
+    tags: 'array-de-tags-da-receita-ou-array-vazio',
+  }];
+
+  localStorage.setItem('doneRecipes', JSON.stringify(doneRecipesData));
+  const doneRecipesGet = localStorage.getItem('doneRecipes');
+  doneRecipesData = JSON.parse(doneRecipesGet);
+
   return (
     <div className="done-recipes">
-      <Header showSearchButton={ false } />
+      <Header
+        showSearchButton={ false }
+        title="Done Recipes"
+      />
       <h1 data-testid="page-title">Done Recipes</h1>
       <div className="main-categories">
         <input
